fb8b92feaadf455dc9a987222dd7cc6aab341a5f
Joao Victor Batista <vbatista.dev@gmail.com>
2026-01-28 14:36:41 -0300

    Ajustado o codigo para o uso do mongoDB localmente

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,8 @@ import {prisma} from "../../databases/orm/lib/prisma.ts"
 const app = express();
 
 app.get("/",async (req,res)=>{
-    const pessoas = await prisma.pessoa.findMany(); 
-    res.status(200).json(pessoas);
+    const Users = await prisma.user.findMany(); 
+    res.status(200).json(Users);
 })
 
 app.listen(3000);
